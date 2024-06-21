3263e4425bb242dacd720ad7a605de5e84e6177a
Nikhil Barik <nklmarch17@gmail.com>
2024-06-21 10:48:42 +0530

    Initial Commit

---
+++ Scanny/ScannyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannyApp.swift
+//  Scanny
+//
+//  Created by Nikhil Barik on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ScannyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
